42a6cdc65e72823c008984433d780bea07a13eed
James Clement <jameseclement@gmail.com>
2019-04-02 11:06:54 -0400

    create portfolios controller, portfolio model, add index and update

---
+++ app/controllers/api/v1/portfolios_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::PortfoliosController < ApplicationController
+  before_action :find_portfolio, only: [:update]
+
+  def index
+    @portfolios = Portfolio.all
+    render json: @portfolios
+  end
+
+  def update(portfolio_params)
+    if @portfolio.save
+      render json: @portfolio, status: :accepted
+    else
+      render json: {errors: @portfolio.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def portfolio_params
+    params.require(:portfolio).permit(:name, :start_date, :end_date, :starting_cash, :number_of_players)
+  end
+
+  def find_portfolio
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+end
---
+++ db/migrate/20190402150444_create_portfolios.rb
@@ -0,0 +1,11 @@
+class CreatePortfolios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :portfolios do |t|
+      t.string :cash
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :game, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
