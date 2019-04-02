a767ee33504887b07981075dd46df7d3e673c027
James Clement <jameseclement@gmail.com>
2019-04-02 12:10:06 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
       namespace :v1 do
         resources :users, only: [:index, :update]
+        resources :games, only: [:index, :update]
+        resources :portfolios, only: [:index, :update]
+        resources :positions, only: [:index, :update]
+        resources :watchistItems, only: [:index, :update]
+        resources :stocks, only: [:index, :update]
       end
     end
   end
