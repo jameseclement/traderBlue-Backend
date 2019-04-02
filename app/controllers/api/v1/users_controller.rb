23ed70d45d65b105f195cc34e96ad6f205439224
James Clement <jameseclement@gmail.com>
2019-04-02 10:37:53 -0400

    add user update method, user_params method, and find_user method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,28 @@
 class Api::V1::UsersController < ApplicationController
   before_action :find_user, only: [:update]
+
   def index
     @users = User.all
     render json: @notes
   end
+
+  def update(user_params)
+    if @user.save
+      render json: @user, status: :accepted
+    else
+      render json: {errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password_digest, :email, :first_name, :last_name)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end 
+
+
+
 end
