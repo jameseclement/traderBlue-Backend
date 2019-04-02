c973ab07e200fa805fed29b49ca77b0857a5b7ed
James Clement <jameseclement@gmail.com>
2019-04-02 19:19:37 -0400

    add positions to user serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :portfolios
   has_many :games, through: :portfolios
+  has_many :positions, through: :portfolios
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :last_name
   has_many :portfolios
   has_many :games
+  has_many :positions
 end
