c7806bb1aaae83b11c2acf7a7a6140b583be185b
James Clement <jameseclement@gmail.com>
2019-04-08 16:46:56 -0400

    watchlist item serializer has news

--- app/serializers/watchlist_item_serializer.rb
+++ app/serializers/watchlist_item_serializer.rb
@@ -1,5 +1,12 @@
 class WatchlistItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :ticker, :cap_level, :industry
+  attributes :id, :name, :ticker, :cap_level, :industry, :news
   belongs_to :portfolio
   # belongs_to :stock
+
+def news
+  url = "https://api.iextrading.com/1.0/stock/#{self.object.ticker}/news/last/5"
+  response = RestClient.get(url)
+
+  return JSON.parse(response)
+end
 end
